83a77214d69747262fd02df2b474eca3a4814782
AI Profit Lab <ai.profit.lab2026@gmail.com>
2026-06-11 05:02:48 -0700

    fixing the corrupted style 110626

---
+++ Website SEO/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public_html/**/*.html",
+    "./public_html/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
